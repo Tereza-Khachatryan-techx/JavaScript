1c8410a7e9fc8431a463cd7e3df7a7d21a217a9a
Tereza-Tereza <tereza.khachatryan.techx@gmail.com>
2025-10-09 19:17:26 +0400

    Formated files

--- Task4/index.js
+++ Task4/index.js
@@ -1,15 +1,15 @@
-const clearBtn = document.querySelector('.clearAll')
-const message = document.querySelector('.message')
+const clearBtn = document.querySelector(".clearAll");
+const message = document.querySelector(".message");
 
-clearBtn.addEventListener('click', () => {
-    localStorage.clear()
-    sessionStorage.clear()
-      document.cookie.split(";").forEach((c) => {
+clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  sessionStorage.clear();
+  document.cookie.split(";").forEach((c) => {
     document.cookie = c
       .replace(/^ +/, "")
       .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
   });
-  console.log('All Storage Cleared!')
-  clearBtn.style.display = 'none'
-  message.textContent = 'All Storage Cleared!'
-})
+  console.log("All Storage Cleared!");
+  clearBtn.style.display = "none";
+  message.textContent = "All Storage Cleared!";
+});
